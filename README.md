6d19ec0d8234d593f86c2c11511f80c9051d9d78
choks <choks0225@naver.com>
2022-02-19 22:47:44 +0900

    SIMPLE Slider Carousel with Frame Motion Tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+SIMPLE Slider Carousel with Frame Motion Tutorial
+
+youtube : https://www.youtube.com/watch?v=W0bEL93tt4k
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel);
+    console.log(carousel.current);
+    console.log(carousel.current.scrollWidth);
+    console.log(carousel.current.offsetWidth);
+
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <motion.h1 animate={{ x: 250 }}>hello</motion.h1> */}
+
+      <motion.div ref={carousel} className="carousel">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
         >
-          Learn React
-        </a>
-      </header>
+          {images.map((image, index) => {
+            return (
+              <motion.div key={index} className="item">
+                <img src={image} alt="" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
